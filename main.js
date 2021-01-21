423d07e1698e1711e0fbf76b723096e3ef5dcb1d
wasiak93 <kacperjacekwasiak@gmail.com>
2021-01-21 10:19:15 +0100

    add setContent function; add logo f1 to assets

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
-fetch('http://ergast.com/api/f1/current/driverStandings.json')
-  .then(resp => resp.json())
-  .then(data => console.log(data))
-  .catch(error => console.log(error))
+const api = 'http://ergast.com/api/f1'
+
+const err = (er) => {
+  console.log(`dupa ${er}`)
+}
+
+const fetchApi = (endpoint) => {
+  return fetch(`${api}${endpoint}`)
+    .then(response => {
+      if(response.ok) {
+        return response}
+      else {
+      err('lol')
+      throw Error(`Http error: ${response.status}`)
+        }
+    })
+    .then(resp => resp.json())
+    .catch(e => err(e))
+}
+
+setContent = (data) => {
+  console.log(data)
+  const season = data.season
+  const round = data.round
+  const driverStandingsArray = data.DriverStandings
+  console.log(season)
+  console.log(round)
+  console.log(driverStandingsArray)
+
+}
+
+fetchApi('/current/driverStandings.json')
+  .then(data => {
+    if(!data) return;
+    setContent(data.MRData.StandingsTable.StandingsLists[0])
+  })
