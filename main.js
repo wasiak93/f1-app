31aa2c22ba59292b91ed76076002b67c3088f4eb
wasiak93 <kacperjacekwasiak@gmail.com>
2021-01-21 10:35:05 +0100

    add setSeason and setRound function

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const api = 'http://ergast.com/api/f1'
+const contentEl = document.querySelector('.content')
 
 const err = (er) => {
   console.log(`dupa ${er}`)
@@ -18,6 +19,24 @@ const fetchApi = (endpoint) => {
     .catch(e => err(e))
 }
 
+setSeason = (season) => {
+  const seasonEl = document.createElement('div')
+  seasonEl.classList.add('content__season-wrapper')
+  seasonEl.innerHTML = `
+  <p class="content__season-text">Season:</p>
+  <p class="content__season-data">${season}</p>`
+  contentEl.appendChild(seasonEl)
+}
+
+setRound = (round) => {
+  const roundEl = document.createElement('div')
+  roundEl.classList.add('content__round-wrapper')
+  roundEl.innerHTML = `
+  <p class="content__round-text">Round:</p>
+  <p class="content__round-data">${round}</p>`
+  contentEl.appendChild(roundEl)
+}
+
 setContent = (data) => {
   console.log(data)
   const season = data.season
@@ -27,6 +46,8 @@ setContent = (data) => {
   console.log(round)
   console.log(driverStandingsArray)
 
+  setSeason(season) 
+  setRound(round) 
 }
 
 fetchApi('/current/driverStandings.json')
